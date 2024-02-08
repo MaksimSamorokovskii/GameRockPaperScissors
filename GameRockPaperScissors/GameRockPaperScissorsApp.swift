d1b9b17d163ddfdf4ff626f120696d19ee9f5f6f
MaksimSamorokovskii <139316826+MaksimSamorokovskii@users.noreply.github.com>
2024-02-08 13:08:23 +0530

    Initial commit

---
+++ GameRockPaperScissors/GameRockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameRockPaperScissorsApp.swift
+//  GameRockPaperScissors
+//
+//  Created by Максим Самороковский on 08.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct GameRockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
